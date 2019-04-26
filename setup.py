214db150831d2e0c9aa917a06a32cce04fa0507e
jenkins <jenkins@nalkins.cloud>
2019-04-26 09:33:24 +0000

    update version to: 0.1.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nalkinscloud-mqtt-simulators",
-    version="0.1.10",
+    version="0.1.11",
     author="Arie Lev",
     author_email="levinson.arie@gmail.com",
     description="Simulate MQTT end devices",
