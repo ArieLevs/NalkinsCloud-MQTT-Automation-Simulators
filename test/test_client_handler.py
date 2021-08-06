cbf6c491a0f62c301e60581ae49eae2d51d47b60
Arie Lev <levinsonarie@gmail.com>
2021-08-06 11:27:56 +0300

    add dummy tests

---
+++ test/test_client_handler.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class FunctionsTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @staticmethod
+    def test_application():
+        print("ADD APPLICATION TESTS")
+
+
+if __name__ == '__main__':
+    unittest.main()
